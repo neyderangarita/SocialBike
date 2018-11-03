e31c6068b159e610976caf16bae71495a8dd88c5
neyder.angarita <neyder6101@hotmail.com>
2018-11-03 17:23:17 +0000

    Agrega cambios en db

--- db/schema.rb
+++ db/schema.rb
@@ -19,14 +19,6 @@ ActiveRecord::Schema.define(version: 20181103154919) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "books", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_books_on_user_id"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string   "comment"
     t.datetime "date"
