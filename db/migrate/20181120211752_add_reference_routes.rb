833427d2a81b4413d5e160877ae6a47f9dd7bb96
neyderangarita <neyder6101@hotmail.com>
2019-01-02 14:19:36 -0500

    Ajuste produccion borra relacion creada event_id

--- db/migrate/20181120211752_add_reference_routes.rb
+++ db/migrate/20181120211752_add_reference_routes.rb
@@ -1,6 +1,6 @@
 class AddReferenceRoutes < ActiveRecord::Migration[5.0]
   def change
-    add_column :routes, :event_id, :integer
-    add_index :routes, :event_id
+    #add_column :routes, :event_id, :integer
+    #add_index :routes, :event_id
   end
 end
